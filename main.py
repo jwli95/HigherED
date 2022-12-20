1edd7978de657bdfe4d4ef56f9ca536149f2c4e6
jwli95 <lijwnss@gmail.com>
2022-12-20 15:20:34 -0700

    dec20

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def run(playwright: Playwright, jobcat: int, pagenum: int, cur_page: int) -> Non
 
 with sync_playwright() as playwright:
     # Find number of ADs
-    filename = 'EE.json'
+    filename = 'CE.json'
 
     if filename == 'CS.json':
         url = 'https://www.higheredjobs.com/faculty/search.cfm?JobCat=102&CatName=Computer%20Science'
